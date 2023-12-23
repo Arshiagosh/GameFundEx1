e49a21716ae40b2aabc719cd61ec4045001807c8
Arshia Goshtasbi <Arshia.goshtasbi@gmail.com>
2023-12-23 22:28:28 +0330

    Car logic added

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class aa : MonoBehaviour
+public class CarScript : MonoBehaviour
 {
+    public float speed;
+
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class aa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(speed*Time.deltaTime,0f,0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-speed*Time.deltaTime,0f,0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(0f,rotationSpeed*Time.deltaTime,0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0f,-rotationSpeed*Time.deltaTime,0f);
+        }
     }
 }
