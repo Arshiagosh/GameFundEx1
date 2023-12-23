0e4afc35159c3ad4f78a038932f0c939a209aa59
Arshia Goshtasbi <Arshia.goshtasbi@gmail.com>
2023-12-23 22:55:31 +0330

    Added lights to the car and its logic

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,16 @@ using UnityEngine;
 public class CarScript : MonoBehaviour
 {
     public float speed;
-
     public float rotationSpeed;
+    public GameObject leftLight;
+    public GameObject rightLight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftLight = GameObject.Find("left");
+        rightLight = GameObject.Find("right");
+        rightLight.SetActive(false);
+        leftLight.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,6 +36,24 @@ public class CarScript : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.Rotate(0f,-rotationSpeed*Time.deltaTime,0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "tunel")
+        {
+            rightLight.SetActive(true);
+            leftLight.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "tunel")
+        {
+            rightLight.SetActive(false);
+            leftLight.SetActive(false);
         }
     }
 }
